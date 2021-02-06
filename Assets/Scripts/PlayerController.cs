9b5daed567062b9076c0b960bdadafd87315bf84
KeitoTakaishi <takaishi78@gmail.com>
2021-02-06 23:25:18 +0900

    Fitst Commit
    Baked Light Experiment

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float speed;
+	[SerializeField] private bool isAutoRotate;
+	[SerializeField] private float rotateSpeed ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		if(Input.GetKey("up"))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if(Input.GetKey("down"))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		if(Input.GetKey("right"))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey("left"))
+		{
+			transform.position -= Vector3.right * speed * Time.deltaTime;
+		}
+
+
+		var t = Time.realtimeSinceStartup;
+		Vector3 r = new Vector3(t, t, t) * rotateSpeed;
+
+		if(isAutoRotate)
+		{
+			this.transform.localEulerAngles = r;
+		}
+	}
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] Vector3 rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float t = Time.realtimeSinceStartup;
+        this.transform.localEulerAngles = rotationSpeed * t;
+    }
+}
